114abff7f5bbd6ca99000c87ebe3f425645e9774
ChabriGanesh <chabriganesh716@gmail.com>
2025-09-26 15:15:12 +0530

    Run

--- infoauto.py
+++ infoauto.py
@@ -5,15 +5,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import classification_report 
 
-@st.cache_data 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FILE_PATH = os.path.join(BASE_DIR, "industrial_fault_detection_data_1000.csv")
 @st.cache_data
 def load_data():
     df = pd.read_csv(FILE_PATH)
     df["Timestamp"] = pd.to_datetime(df["Timestamp"])
-return df
-df = load_data() 
+    return df
+df = load_data()
 df.columns = df.columns.str.strip().str.replace("Â", "", regex=False) 
 
 st.title("🏭 Manufacturing IoT - Intelligent Automation Dashboard") 
